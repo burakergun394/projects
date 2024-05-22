be201f309860828005c1588f0e3225936a4c2bfe
burakergun394 <burakergun394@gmail.com>
2024-05-22 21:57:25 +0300

    added core library and api projects

---
+++ TourismVehicleService/back-end/aspnetcore/src/Presentation.Web.API.Controllers/Configuration/MvcBuilderConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Presentation.Web.API.Controllers.Configuration;
+
+public static class MvcBuilderConfiguration
+{
+    public static IMvcBuilder AddPresentationControllers(this IMvcBuilder builder)
+    {
+        Assembly currentAssembly = Assembly.GetExecutingAssembly();
+
+        builder
+            .AddApplicationPart(currentAssembly);
+
+        return builder;
+    }
+}
